232b9c5d34fc90f5e2747a1337f71f27f4524d37
Luccas Aguilar <luccasaguilar@gmail.com>
2025-07-18 13:58:56 -0400

    js and conditions

--- scripts/place.js
+++ scripts/place.js
@@ -1,9 +1,7 @@
 function calculateWindChill(temp, speed, isMetric) {
-  if (isMetric) {
-    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
-  } else {
-    return 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-  }
+  return isMetric
+    ? 13.12 + 0.6215 * temp - 11.37 * speed ** 0.16 + 0.3965 * temp * speed ** 0.16
+    : 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
 }
 
 function displayWindChill() {
