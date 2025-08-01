6c7ad03e30dbed02b4f7c208014b7e5068f5d0f1
Luccas Aguilar <luccasaguilar@gmail.com>
2025-08-01 00:47:56 -0400

    js

--- week05/scripts/form.js
+++ week05/scripts/form.js
@@ -1,8 +1,30 @@
 // Product array
 const products = [
-    { id: 1, name: 'Product A' },
-    { id: 2, name: 'Product B' },
-    { id: 3, name: 'Product C' }
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
